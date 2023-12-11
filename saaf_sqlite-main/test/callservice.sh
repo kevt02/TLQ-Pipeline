1a04d308aa0d552cbc88f3539949545fa8ee0a3f
kevt02 <kht5832@gmail.com>
2023-12-11 11:25:44 -0800

    with test

---
+++ saaf_sqlite-main/test/callservice.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# JSON object to pass to Lambda Function
+
+json='{
+  "bucketname": "records-462",
+  "filename": "100SalesRecords.csv",
+  "filters": {
+    "Region": "Asia"
+  },
+  "aggregations": ["avg(OrderProcessingTime)", "avg(GrossMargin)"]
+}'
+
+#echo "Invoking Lambda function using API Gateway"
+
+#time output=`curl -s -H "Content-Type: application/json" -X POST -d $json https://lfbckqyqta.execute-api.us-east-2.amazonaws.com/processWithARM`
+
+#echo ""
+
+
+
+#echo ""
+
+#echo "JSON RESULT:"
+
+#echo $output | jq
+
+#echo ""
+
+
+
+
+echo "Invoking Lambda function using AWS CLI"
+
+time output=`aws lambda invoke --invocation-type RequestResponse --function-name processRecordsARM --region us-east-2 --payload "$json" /dev/stdout | head -n 1 | head -c -2 ; echo`
+
+echo ""
+
+echo "JSON RESULT:"
+
+echo $output | jq
+
+echo ""
+
+
+
+
