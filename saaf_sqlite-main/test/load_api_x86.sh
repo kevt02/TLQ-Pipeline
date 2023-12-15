ee340e8520480b279322f35e0d5fd27a389e6426
karan <karandeepsangha95@gmail.com>
2023-12-15 09:19:40 -0800

    fix api

---
+++ saaf_sqlite-main/test/load_api_x86.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# JSON object to pass to Lambda Function
+
+json='{
+  "bucketname": "records-462",
+  "filename": "100SalesRecords.csv",
+  "filters": {
+    "Region": "Asia"
+  },
+  "aggregations": ["avg(OrderProcessingTime)", "avg(GrossMargin)"]
+}'
+
+
+
+echo "Invoking Lambda function using API Gateway"
+time output=$(curl -s -H "Content-Type: application/json" -X POST -d "$json" https://luxa8b44lc.execute-api.us-east-2.amazonaws.com/load_deploy)
+
+echo ""
+echo "JSON RESULT:"
+echo $output | jq
+echo ""
+
+
+
+
+#echo "Invoking Lambda function using AWS CLI"
+
+#time output=`aws lambda invoke --invocation-type RequestResponse --function-name queryRecords --#region us-east-2 --payload "$json" /dev/stdout | head -n 1 | head -c -2 ; echo`
+
+#echo ""
+
+#echo "JSON RESULT:"
+
+#echo $output | jq
+
+#echo ""
+
+
+
+
+
+
